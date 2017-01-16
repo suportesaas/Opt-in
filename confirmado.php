3b23bd3b430dfeb8107eb059f2363cbb2b8a51c6
nicolas gleiser <nicolas.lrd@hotmail.com>
2017-01-16 17:43:38 -0200

    Remoção de aparencias

--- confirmado.php
+++ confirmado.php
@@ -1,28 +1,12 @@
 <html>
 <head>
 <?php ini_set('session.use_cookies', '0'); ?>
-<meta charset="utf-8">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 
-<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-<?php include "navbar.php"; ?>
-<div class="col-md-4"> </div>
-<div class="col-md-4">
-<div class="alert alert-success">
-                <a href="index.php"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-                    ×</button></a>
-               <span class="glyphicon glyphicon-ok"></span> <strong>Confirmado</strong>
-                <hr class="message-inner-separator">
-                <h1>
-                    Parabéns, você esta cadastrado em nossa Newssletter!.</h1>
-            </div>
 
-</div><div class="col-md-4"> </div>
+    <h1>Parabéns, você esta cadastrado em nossa Newssletter!.</h1>
+           
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,30 +3,19 @@
 <head>
 <?php ini_set('session.use_cookies', '0'); ?>
 	<title>
-		Teste Opt-in
+		Formulário de Cadstro ativo
 	</title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+	
 </head>
 <body>
-<?php include "navbar.php"; ?>
-<div class="col-md-4"> </div>
-<div class="col-md-4">
-<div class="humbnail center well well-small text-center">
 	<form action="cadastrar.php" class="form-group" method="POST">
 		<h4>Cadastre-se Aqui para receber nossas newsletter</h4>
 		<input type="text" name="name" placeholder="Digite Seu nome" required>
 		<input type="email" name="email" placeholder="Digite aqui seu e-mail" required>
-		<input type="submit" value="Cadastrar" class="btn btn-info btn-lg">
-	</form>
-	</div>
-<div class="col-md-4"> </div>
+	<input type="submit" value="Cadastrar" class="btn btn-info btn-lg">
+</form>
+	
 </div>
 </body>
 </html>
